9cd6d7a1d9f5de82fafc771779d55bd3f6d99927
Gerob617 <18196578@studentmail.ul.ie>
2020-02-12 11:33:15 +0000

    Update main.lua

--- BalloonTap/main.lua
+++ BalloonTap/main.lua
@@ -17,7 +17,7 @@ local platform = display.newImageRect( "Hurley.png", 300, 50 )
 platform.x = display.contentCenterX
 platform.y = display.contentHeight-25
 
-local balloon = display.newImageRect( "Sliotar.png", 112, 112 )
+local balloon = display.newImageRect( "balloon.png", 112, 112 )
 balloon.x = display.contentCenterX
 balloon.y = display.contentCenterY
 balloon.alpha = 0.8
