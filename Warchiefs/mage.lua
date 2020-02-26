ff7960aaf5918384723d61fb5f93daabcc3773d0
Jim Ryan <jimmih15@gmail.com>
2020-02-26 11:39:36 +0000

    delete comment

--- Warchiefs/mage.lua
+++ Warchiefs/mage.lua
@@ -2,19 +2,8 @@ local composer = require("composer")
 
 local scene = composer.newScene()
 
--- -----------------------------------------------------------------------------------
--- Code outside of the scene event functions below will only be executed ONCE unless
--- the scene is removed entirely (not recycled) via "composer.removeScene()"
--- -----------------------------------------------------------------------------------
-
--- -----------------------------------------------------------------------------------
--- Scene event functions
--- -----------------------------------------------------------------------------------
-
--- create()
 function scene:create(event)
     local sceneGroup = self.view
-    -- Code here runs when the scene is first created but has not yet appeared on screen
 end
 
 -- show()
