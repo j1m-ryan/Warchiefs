5ad339400369eb86c3551778c2c23278605db5e7
Harn33t <58052846+Harn33t@users.noreply.github.com>
2020-04-02 01:34:55 +0100

    attackDelay, attack func broken down in min funcs

--- Warchiefs/TestData2.lua
+++ Warchiefs/TestData2.lua
@@ -1,7 +1,7 @@
 local enemy = {}
 enemy.id = "enemy"
 enemy.health = 1000
-enemy.weapon = 25
+enemy.damage = 25
 enemy.level = 1
 enemy.totalHealth = 1000
 return enemy
--- Warchiefs/playerData.lua
+++ Warchiefs/playerData.lua
@@ -17,7 +17,7 @@ player.x = 30
 player.y = 30
 player.location = ""
 player.health = 300 --default
-player.weapon = 150
+player.damage = 150
 player.level = 1
 player.evade = 3
 
