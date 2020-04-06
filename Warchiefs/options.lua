991314cd386e6f9c5e739fbb2ae4bc056f1c5aa9
Jim Ryan <jimmih15@gmail.com>
2020-04-07 00:39:06 +0100

    scene groups for options.lua

--- Warchiefs/options.lua
+++ Warchiefs/options.lua
@@ -1,95 +1,84 @@
 local composer = require("composer")
 local scene = composer.newScene()
 
-
-
--- show()
-function scene:show(event)
+function scene:create(event)
     local sceneGroup = self.view
+
     local phase = event.phase
 
-    if (phase == "will") then
-        -- Code here runs when the scene is still off screen (but is about to come on screen)
-        local widget = require("widget")
+    local widget = require("widget")
 
-        -- Set background image
-        local background = display.newImageRect("images/background.png", 1280, 720)
-        background.x = display.contentCenterX
-        background.y = display.contentCenterY
+    -- Set background image
+    local background = display.newImageRect("images/background.png", 1280, 720)
+    background.x = display.contentCenterX
+    background.y = display.contentCenterY
 
-        -- Function to go to the menu scene
+    -- Function to go to the menu scene
 
-        local function goToMenu(event)
-            if ("ended" == event.phase) then
-                composer.gotoScene("menu", {effect = "crossFade", time = 500})
-            end
+    local function goToMenu(event)
+        if ("ended" == event.phase) then
+            composer.gotoScene("menu", {effect = "crossFade", time = 500})
         end
-
-        local function volumeUp(event)
-            if ("ended" == event.phase) then
-                audio.setVolume(audio.getVolume() + 0.1)
-            end
-        end
-
-        local function volumeDown(event)
-            if ("ended" == event.phase) then
-                audio.setVolume(audio.getVolume() - 0.1)
-            end
-        end
-
-        -- Create the Buttons
-        local volUp =
-            widget.newButton(
-            {
-                width = 250,
-                height = 75,
-                left = 500,
-                top = 380,
-                id = "volUp",
-                defaultFile = "images/volumeupbutton.png",
-                overFile = "images/voluphover.png",
-                onEvent = volumeUp
-            }
-        )
-
-        local volDown =
-            widget.newButton(
-            {
-                width = 250,
-                height = 75,
-                left = 500,
-                top = 490,
-                id = "volDown",
-                defaultFile = "images/volumedownbutton.png",
-                overFile = "images/voldownhover.png",
-                onEvent = volumeDown
-            }
-        )
-
-        local returnMain =
-            widget.newButton(
-            {
-                width = 250,
-                height = 75,
-                left = 500,
-                top = 600,
-                id = "returnmain",
-                defaultFile = "images/mainbutton.png",
-                overFile = "images/returnmainhover.png",
-                onEvent = goToMenu
-
-            }
-        )
-
-
-    elseif (phase == "did") then
-    -- Code here runs when the scene is entirely on screen
-
-    --play bg music
-    audio.play (menuMusic, {channel = 1, loops=-1})
-
-
     end
+
+    local function volumeUp(event)
+        if ("ended" == event.phase) then
+            audio.setVolume(audio.getVolume() + 0.1)
+        end
+    end
+
+    local function volumeDown(event)
+        if ("ended" == event.phase) then
+            audio.setVolume(audio.getVolume() - 0.1)
+        end
+    end
+
+    -- Create the Buttons
+    local volUp =
+        widget.newButton(
+        {
+            width = 250,
+            height = 75,
+            left = 500,
+            top = 380,
+            id = "volUp",
+            defaultFile = "images/volumeupbutton.png",
+            overFile = "images/voluphover.png",
+            onEvent = volumeUp
+        }
+    )
+
+    local volDown =
+        widget.newButton(
+        {
+            width = 250,
+            height = 75,
+            left = 500,
+            top = 490,
+            id = "volDown",
+            defaultFile = "images/volumedownbutton.png",
+            overFile = "images/voldownhover.png",
+            onEvent = volumeDown
+        }
+    )
+
+    local returnMain =
+        widget.newButton(
+        {
+            width = 250,
+            height = 75,
+            left = 500,
+            top = 600,
+            id = "returnmain",
+            defaultFile = "images/mainbutton.png",
+            overFile = "images/returnmainhover.png",
+            onEvent = goToMenu
+        }
+    )
+    sceneGroup:insert(background)
+    sceneGroup:insert(returnMain)
+    sceneGroup:insert(volDown)
+    sceneGroup:insert(volUp)
 end
 
 -- hide()
