e17228b937c8306c46e205e59b9e9c6594fe4f38
Jim Ryan <jimmih15@gmail.com>
2020-04-06 11:32:49 +0100

    Update city3.lua

--- Warchiefs/city3.lua
+++ Warchiefs/city3.lua
@@ -15,97 +15,99 @@ local scene = composer.newScene()
 
 -- create()
 function scene:create(event)
-    local sceneGroup = self.view
-    -- Code here runs when the scene is first created but has not yet appeared on screen
-end
-
--- show()
-function scene:show(event)
+    composer.removeHidden()
     player.location = "city3"
     local sceneGroup = self.view
     local phase = event.phase
 
-    if (phase == "will") then
-        -- Create the widget
-        -- Code here runs when the scene is still off screen (but is about to come on screen)
-        local background = display.newImageRect("images/city3inside.png", 1280, 720)
-        background.x = display.contentCenterX
-        background.y = display.contentCenterY
+    -- Create the widget
+    -- Code here runs when the scene is still off screen (but is about to come on screen)
+    local background = display.newImageRect("images/city3inside.png", 1280, 720)
+    background.x = display.contentCenterX
+    background.y = display.contentCenterY
 
-        local widget = require("widget")
+    local widget = require("widget")
 
-        -- Function to handle button events
-        local function goToWorldMap(event)
-            if ("ended" == event.phase) then
-                composer.gotoScene("worldmap")
-            end
+    -- Function to handle button events
+    local function goToWorldMap(event)
+        if ("ended" == event.phase) then
+            composer.gotoScene("worldmap")
         end
-        local function goToLvl(event)
-            if ("ended" == event.phase) then
-                composer.showOverlay("level")
-            end
-        end
-        local function goToMage(event)
-            if ("ended" == event.phase) then
-                composer.showOverlay("mage")
-            end
-        end
-        local function goToCastle(event)
-            if ("ended" == event.phase) then
-                composer.showOverlay("combat")
-            end
-        end
-        local exitRyansTown =
-            widget.newButton(
-            {
-                left = 1030,
-                top = 500,
-                width = 200,
-                height = 190,
-                id = "exitRyansTown",
-                onEvent = goToWorldMap,
-                defaultFile = "images/door.png"
-            }
-        )
-        local lvlScreen =
-            widget.newButton(
-            {
-                defaultFile = "images/market.png",
-                left = 130,
-                top = 350,
-                width = 250,
-                height = 200,
-                id = "lvlScreen",
-                onEvent = goToLvl
-            }
-        )
-
-        local mageBtn =
-            widget.newButton(
-            {
-                width = 100,
-                height = 300,
-                left = 785,
-                top = 30,
-                id = "magebtn",
-                defaultFile = "images/tower.png",
-                onEvent = goToMage
-            }
-        )
-        local castleBtn =
-            widget.newButton(
-            {labelColor = { default={ 1, 1, 1 }, over={ 0, 0, 0, 0.5 } },
-                left = 925,
-                top = 130,
-                id = "castleBtn",
-                label = "castle",
-                fontSize=40,
-                onEvent = goToCastle
-            }
-        )
-    elseif (phase == "did") then
-    -- Code here runs when the scene is entirely on screen
     end
+    local function goToLvl(event)
+        if ("ended" == event.phase) then
+            composer.showOverlay("level")
+        end
+    end
+    local function goToMage(event)
+        if ("ended" == event.phase) then
+            composer.showOverlay("mage")
+        end
+    end
+    local function goToCastle(event)
+        if ("ended" == event.phase) then
+            composer.showOverlay("combat")
+        end
+    end
+    local exitRyansTown =
+        widget.newButton(
+        {
+            left = 1030,
+            top = 500,
+            width = 200,
+            height = 190,
+            id = "exitRyansTown",
+            onEvent = goToWorldMap,
+            defaultFile = "images/door.png"
+        }
+    )
+    local lvlScreen =
+        widget.newButton(
+        {
+            defaultFile = "images/market.png",
+            left = 130,
+            top = 350,
+            width = 250,
+            height = 200,
+            id = "lvlScreen",
+            onEvent = goToLvl
+        }
+    )
+
+    local mageBtn =
+        widget.newButton(
+        {
+            width = 100,
+            height = 300,
+            left = 785,
+            top = 30,
+            id = "magebtn",
+            defaultFile = "images/tower.png",
+            onEvent = goToMage
+        }
+    )
+    local castleBtn =
+        widget.newButton(
+        {
+            labelColor = {default = {1, 1, 1}, over = {0, 0, 0, 0.5}},
+            left = 925,
+            top = 130,
+            id = "castleBtn",
+            label = "castle",
+            fontSize = 40,
+            onEvent = goToCastle
+        }
+    )
+    sceneGroup:insert(background)
+    sceneGroup:insert(castleBtn)
+    sceneGroup:insert(mageBtn)
+    sceneGroup:insert(lvlScreen)
+
+    sceneGroup:insert(exitRyansTown)
+end
+
+-- show()
+function scene:show(event)
 end
 
 -- hide()
