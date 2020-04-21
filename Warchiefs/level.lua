583a6621a18fd0fd9e7cd93e8cbadabee1484f1d
Jim Ryan <jimmih15@gmail.com>
2020-04-21 20:22:06 +0100

    market

--- Warchiefs/level.lua
+++ Warchiefs/level.lua
@@ -39,13 +39,165 @@ function scene:create(event)
             fontSize = 40
         }
     )
-    local myName = display.newText(player.name, 1010, 75, native.systemFont, 35)
-    local myClan = display.newText(player.clan, 1010, 100, native.systemFont, 35)
+    local myName = display.newText(player.name, 1005, 67, native.systemFont, 35)
+    local myClan = display.newText(player.clan, 1005, 103, native.systemFont, 35)
+
+    local myStr = display.newText(player.strength, 1170, 255, native.systemFont, 35)
+    myStr.anchorX = 0
+    myStr:setFillColor(0, 0, 0)
+
+    local myDxt = display.newText(player.dexterity, 1170, 330, native.systemFont, 35)
+    myDxt.anchorX = 0
+    myDxt:setFillColor(0, 0, 0)
+    local myAgi = display.newText(player.agility, 1170, 390, native.systemFont, 35)
+    myAgi.anchorX = 0
+    myAgi:setFillColor(0, 0, 0)
+    local myChr = display.newText(player.charisma, 1170, 460, native.systemFont, 35)
+    myChr.anchorX = 0
+    myChr:setFillColor(0, 0, 0)
+    local myGold = display.newText(player.gold, 1100, 530, native.systemFont, 35)
+    myGold.anchorX = 0
+    myGold:setFillColor(.8, .6, .2)
+
+    myName.anchorX = 0
+    myClan.anchorX = 0
+
+    -- Function to handle button events
+    local function handleSharpen(event)
+        if (event.phase == "ended" and player.gold >= 1000) then
+            myGold:removeSelf()
+            myStr:removeSelf()
+
+            player.gold = player.gold - 1000
+            player.strength = player.strength + 1
+            myStr = display.newText(player.strength, 1170, 255, native.systemFont, 35)
+            myStr.anchorX = 0
+            myStr:setFillColor(0, 0, 0)
+            myGold = display.newText(player.gold, 1100, 530, native.systemFont, 35)
+            myGold.anchorX = 0
+            myGold:setFillColor(.8, .6, .2)
+            sceneGroup:insert(myStr)
+            sceneGroup:insert(myGold)
+        end
+    end
+
+    local function handleDrink(event)
+        if (event.phase == "ended" and player.gold >= 1000) then
+            myGold:removeSelf()
+            myChr:removeSelf()
+
+            player.gold = player.gold - 1000
+            player.charisma = player.charisma + 1
+            myChr = display.newText(player.charisma, 1170, 460, native.systemFont, 35)
+            myChr.anchorX = 0
+            myChr:setFillColor(0, 0, 0)
+            myGold = display.newText(player.gold, 1100, 530, native.systemFont, 35)
+            myGold.anchorX = 0
+            myGold:setFillColor(.8, .6, .2)
+            sceneGroup:insert(myChr)
+            sceneGroup:insert(myGold)
+        end
+    end
+
+    local function handleTrain(event)
+        if (event.phase == "ended" and player.gold >= 1000) then
+            myGold:removeSelf()
+            myAgi:removeSelf()
+
+            player.gold = player.gold - 1000
+            player.agility = player.agility + 1
+            myAgi.text = player.agility
+            myGold = display.newText(player.gold, 1100, 530, native.systemFont, 35)
+            myGold.anchorX = 0
+            myGold:setFillColor(.8, .6, .2)
+
+            sceneGroup:insert(myGold)
+            sceneGroup:insert(myAgi)
+        end
+    end
+    local function handleDex(event)
+        if (event.phase == "ended" and player.gold >= 1000) then
+            myGold:removeSelf()
+            myDxt:removeSelf()
+
+            player.gold = player.gold - 1000
+            player.dexterity = player.dexterity + 1
+            myDxt.text = player.dexterity
+            myGold = display.newText(player.gold, 1100, 530, native.systemFont, 35)
+            myGold.anchorX = 0
+            myGold:setFillColor(.8, .6, .2)
+
+            sceneGroup:insert(myGold)
+            sceneGroup:insert(myDxt)
+        end
+    end
+
+    local button1 =
+        widget.newButton(
+        {
+            width = 240,
+            height = 120,
+            defaultFile = "images/sharpenDefault.png",
+            overFile = "images/sharpenOver.png",
+            onEvent = handleSharpen
+        }
+    )
+
+    local button2 =
+        widget.newButton(
+        {
+            width = 240,
+            height = 120,
+            defaultFile = "images/drinkDefault.png",
+            overFile = "images/drinkOver.png",
+            onEvent = handleDrink
+        }
+    )
+    local button3 =
+        widget.newButton(
+        {
+            width = 240,
+            height = 120,
+            defaultFile = "images/trainDefault.png",
+            overFile = "images/trainOver.png",
+            onEvent = handleTrain
+        }
+    )
+    local button4 =
+        widget.newButton(
+        {
+            width = 240,
+            height = 120,
+            defaultFile = "images/dexDefault.png",
+            overFile = "images/dexOver.png",
+            onEvent = handleDex
+        }
+    )
+
+    button1.x = 200
+    button1.y = 300
+    button2.x = 600
+    button2.y = 300
+    button3.x = 200
+    button3.y = 500
+    button4.x = 600
+    button4.y = 500
+
+    myName:setFillColor(0, 0, 0)
+    myClan:setFillColor(0, 0, 0)
     sceneGroup:insert(background)
     sceneGroup:insert(backBTN)
     sceneGroup:insert(myName)
-
+    sceneGroup:insert(myStr)
+    sceneGroup:insert(myDxt)
+    sceneGroup:insert(myAgi)
+    sceneGroup:insert(myChr)
     sceneGroup:insert(myClan)
+    sceneGroup:insert(myGold)
+    sceneGroup:insert(button1)
+    sceneGroup:insert(button2)
+    sceneGroup:insert(button3)
+    sceneGroup:insert(button4)
 end
 
 -- show()
